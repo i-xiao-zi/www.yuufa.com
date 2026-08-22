a0fbf7ef8eb663473abbe0f950d8103a49fffb25
work <work@ixiaozi.cn>
2026-08-22 17:16:49 +0800

    *

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { ActionIcon, CloseButton, Input, Popover, PopoverDropdown, PopoverTarget, SegmentedControl, Tabs, TabsList, TabsPanel, TabsTab } from "@mantine/core";
+import { ActionIcon, CloseButton, Container, Input, Popover, PopoverDropdown, PopoverTarget, SegmentedControl, Tabs, TabsList, TabsPanel, TabsTab } from "@mantine/core";
 import { IconSearch, IconSettings } from "@tabler/icons-react";
 import ContextMenu from "../components/cm";
 import { Searchor } from "@/api/types";
@@ -25,26 +25,25 @@ export default function Page() {
     setSegmente(tab?.searchors?.[0]);
   }, [tab]);
   return (
-    <main className="container mx-auto flex-auto z-1">
-      <Tabs
-        className="w-3xl mt-5 mx-auto" 
-        variant="outline" 
-        color="teal"
-        value={tab?.name}
-        onChange={v => setTab(tabs.find(i => i.name == v))}
-      >
-        <TabsList>
-          {tabs.map(item => <TabsTab key={item.id} value={item.name}>{item.name}</TabsTab>)}
-          <ActionIcon className="absolute! right-0" variant="subtle"><IconSettings /></ActionIcon>
-        </TabsList>
-        {
-          tabs.map(item => (
+    <main className="flex-auto z-1">
+      <Container size="lg" my="5">
+        <Tabs
+          variant="outline" 
+          color="teal"
+          value={tab?.name}
+          onChange={v => setTab(tabs.find(i => i.name == v))}
+        >
+          <TabsList>
+            {tabs.map(item => <TabsTab key={item.id} value={item.name}>{item.name}</TabsTab>)}
+            <ActionIcon className="absolute! right-0" variant="subtle"><IconSettings /></ActionIcon>
+          </TabsList>
+          {tabs.map(item => (
             <TabsPanel className="border border-t-0 p-0! border-(--tab-border-color)" key={item.id} value={item.name} pt="xs">
               <Input
-              classNames={{
-                input: '!border-none pl-3',
-                section: '!border-none',
-              }}
+                classNames={{
+                  input: '!border-none pl-3',
+                  section: '!border-none',
+                }}
                 size="lg"
                 value={value}
                 className="border-0"
@@ -56,7 +55,7 @@ export default function Page() {
                 leftSection={
                   <Popover position="bottom-end" withArrow shadow="md">
                     <PopoverTarget>
-                      <ActionIcon className="self-stretch flex-auto h-[unset]! active:transform-none!">
+                      <ActionIcon bdrs="0" className="self-stretch flex-auto h-[unset]! active:transform-none!">
                         <img src={`${process.env.NEXT_PUBLIC_BASE_STORAGE}/${segmente?.icon}`} />
                       </ActionIcon>
                     </PopoverTarget>
@@ -73,20 +72,16 @@ export default function Page() {
                   </Popover>
                 }
                 rightSection={<>
-                    {value &&<CloseButton aria-label="Clear input" onClick={() => setValue('')} /> }
+                    {value &&<CloseButton onClick={() => setValue('')} /> }
                     <ActionIcon className="self-stretch flex-auto h-[unset]! w-12!" onClick={onSearch}><IconSearch size={25} /></ActionIcon>
                 </>}
                 />
             </TabsPanel>
-          ))
-        }
-        <TabsPanel className="border border-t-0 border-(--tab-border-color)" value="account" pt="xs">
-          Second tab color is blue, it gets this value from props, props have the priority and will
-          override context value
-        </TabsPanel>
-      </Tabs>
-      <hr />
-      <ContextMenu>xxx</ContextMenu>
+          ))}
+        </Tabs>
+      </Container>
+      <Container size="xl">
+      </Container>
     </main>
   );
 }
--- app/ynp/index.tsx
+++ app/ynp/index.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import React from "react";
-import {Avatar, Box, Button, Card, Container, Flex, Group, Input, List, Popover, SegmentedControl, Table, ThemeIcon} from "@mantine/core";
+import {Avatar, Box, Button, Card, Container, Flex, Group, Input, List, Popover, SegmentedControl, Table, ThemeIcon, Typography} from "@mantine/core";
 import { IconCircleCheck, IconCircleDashed } from "@tabler/icons-react";
 import { Calendar } from "@mantine/dates";
 import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import "dayjs/locale/zh-cn";
 import api from "@/api";
 import { YNP } from "@/api/types";
-
+dayjs.extend(utc);
 
 export default function Page() {
   const [tokens, setTokens] = React.useState<YNP.Token[]>([]);
@@ -91,8 +93,9 @@ export default function Page() {
                         </ThemeIcon>
                       )}>
                         <div className={`${['VERIFY_PROXY', 'VERIFY_DIRECT', 'VERIFY_COUNTY_SERV', 'ZN_MILK_CONSUME', 'ZN_PRODUCT_CONSUME', 'TASK_INVITE', 'TASK_ZN_HD'].includes(task.taskType) && 'line-through'}`}>
-                          <span>{task.taskName}</span>
-                          <span className="text-sm text-gray-500">{task.taskDes}</span>
+                          <Typography component="span">{task.taskName}</Typography>
+                          <Typography component="span" mx="sm">({task.allTimes ? task.finishTimes : task.isFinish}/{task.allTimes ? task.allTimes : 1})</Typography>
+                          <Typography component="span" fz="sm" c="gray.6">{task.taskDes}</Typography>
                         </div>
                         <div>
                           {task.isFinish ? (<Button size="xs" disabled>已完成</Button>) : (
@@ -107,52 +110,61 @@ export default function Page() {
                   ))}
                 </List>
               </Box>
-              <Flex className="max-md:w-full max-md:mx-auto" justify="center">
-                <Calendar
-                  size="xl"
-                  locale={'zh-CN'}
-                  getDayProps={(date) => {
-                    const today = dayjs().isSame(date, 'day') ? 'bg-red-300/10!' : '';
-                    const draw_log = drawLogs.find(log => dayjs.unix(log.createTime).isSame(date, 'day'));
-                    const draw_day = draw_log ? 'relative before:content-["*"] before:absolute before:block before:w-full before:h-full before:-z-1': '';
-                    return {
-                      className: `${today} ${draw_day} bg-blue-100/20!`
-                    }
-                  }}
-                  renderDay={(date) => {
-                    const day = dayjs(date).date();
-                    const draw_logs = drawLogs.filter(log => dayjs.unix(log.createTime).isSame(date, 'day')).sort((a, b) => a.createTime - b.createTime);
-                    const growth_logs = growthLogs.filter(log => dayjs.unix(log.createTime).isSame(date, 'day')).sort((a, b) => a.createTime - b.createTime);
-                    return draw_logs.length + growth_logs.length === 0 
-                    ? (<div className="w-full h-full flex items-center justify-center">{day}</div>)
-                    : (<Popover width={400} trapFocus withArrow shadow="md">
-                      <Popover.Target>
-                        <div className="w-full h-full flex items-center justify-center">{day}</div>
-                      </Popover.Target>
-                      <Popover.Dropdown>
-                        <Table>
-                          <Table.Tbody>
-                            {draw_logs.map((log, index) => (
-                              <Table.Tr className="bg-red-300/10!" key={index}>
-                                <Table.Th>补贴</Table.Th>
-                                <Table.Td>{log.des}</Table.Td>
-                                <Table.Td>{log.amount}</Table.Td>
-                                <Table.Td>{dayjs.unix(log.createTime).format('HH:mm')}</Table.Td>
-                              </Table.Tr>
-                            ))}
-                            {growth_logs.map((log, index) => (
-                              <Table.Tr className="bg-blue-300/10!" key={index}>
-                                <Table.Th>成长</Table.Th>
-                                <Table.Td>{log.typeName}</Table.Td>
-                                <Table.Td>{log.growth}</Table.Td>
-                                <Table.Td>{dayjs.unix(log.createTime).format('HH:mm')}</Table.Td>
-                              </Table.Tr>
-                            ))}
-                          </Table.Tbody>
-                        </Table>
-                      </Popover.Dropdown>
-                    </Popover>)
-                  }}/>
+              <Flex className="w-130 max-md:w-full max-md:mx-auto" justify="center">
+                  <Calendar
+                    fullWidth
+                    locale='zh-cn'
+                    getDayProps={(date) => {
+                      const today = dayjs().isSame(date, 'day') ? 'bg-red-300/10!' : '';
+                      const draw_log = drawLogs.find(log => dayjs.unix(log.createTime).isSame(date, 'day'));
+                      const draw_day = draw_log ? 'relative before:content-["*"] before:absolute before:block before:w-full before:h-full before:-z-1': '';
+                      return {
+                        className: `${today} ${draw_day} bg-blue-100/20!`
+                      }
+                    }}
+                    renderDay={(date) => {
+                      const day = dayjs(date).date();
+                      const draw_logs = drawLogs.filter(log => dayjs.unix(log.createTime).isSame(date, 'day')).sort((a, b) => a.createTime - b.createTime);
+                      const growth_logs = growthLogs.filter(log => dayjs.unix(log.createTime).isSame(date, 'day')).sort((a, b) => a.createTime - b.createTime);
+                      const zhunong_logs = zhunongLogs.filter(log => dayjs.unix(log.createTime).isSame(date, 'day')).sort((a, b) => a.createTime - b.createTime);
+                      return draw_logs.length + growth_logs.length + zhunong_logs.length === 0 
+                      ? (<div className="w-full h-full flex items-center justify-center">{day}</div>)
+                      : (<Popover width={400} trapFocus withArrow shadow="md">
+                        <Popover.Target>
+                          <div className="w-full h-full flex items-center justify-center">{day}</div>
+                        </Popover.Target>
+                        <Popover.Dropdown>
+                          <Table>
+                            <Table.Tbody>
+                              {draw_logs.map((log, index) => (
+                                <Table.Tr className="bg-red-300/10!" key={index}>
+                                  <Table.Th>补贴</Table.Th>
+                                  <Table.Td>{log.des}</Table.Td>
+                                  <Table.Td>{log.amount}</Table.Td>
+                                  <Table.Td>{dayjs.unix(log.createTime).format('HH:mm')}</Table.Td>
+                                </Table.Tr>
+                              ))}
+                              {growth_logs.map((log, index) => (
+                                <Table.Tr className="bg-blue-300/10!" key={index}>
+                                  <Table.Th>成长</Table.Th>
+                                  <Table.Td>{log.typeName}</Table.Td>
+                                  <Table.Td>{log.growth}</Table.Td>
+                                  <Table.Td>{dayjs.unix(log.createTime).format('HH:mm')}</Table.Td>
+                                </Table.Tr>
+                              ))}
+                              {zhunong_logs.map((log, index) => (
+                                <Table.Tr className="bg-blue-300/10!" key={index}>
+                                  <Table.Th>助农</Table.Th>
+                                  <Table.Td>{log.des}</Table.Td>
+                                  <Table.Td>{log.amount}</Table.Td>
+                                  <Table.Td>{dayjs.unix(log.createTime).format('HH:mm')}</Table.Td>
+                                </Table.Tr>
+                              ))}
+                            </Table.Tbody>
+                          </Table>
+                        </Popover.Dropdown>
+                      </Popover>)
+                    }}/>
               </Flex>
             </Flex>
           </Box>
